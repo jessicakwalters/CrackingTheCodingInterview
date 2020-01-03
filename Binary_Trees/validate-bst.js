5d248fd289960abc714a58758add15b8b228f881
jessicakwalters <jessicakwalters@gmail.com>
2020-01-02 20:49:55 -0800

    validate bst

---
+++ Binary_Trees/validate-bst.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const BinaryTree = require('./Binary-Tree');
+const BTNode = require('./Node');
+
+function isBST(node){
+  
+}
+
+let tree = new BinaryTree();
+let ten = new BTNode(10);
+tree.root = ten;
+let twenty = new BTNode(20);
+let thirty = new BTNode(30);
+let forty = new BTNode(40);
+let one = new BTNode(1);
+let two = new BTNode(2);
+let three = new BTNode(3);
+let four = new BTNode(4);
+
+ten.right = one;
+ten.left = twenty;
+one.right = two;
+// two.left = three;
+// three.right = four;
+twenty.right = thirty;
+twenty.left = forty;
+
+console.log(isBalanced(tree.root));
