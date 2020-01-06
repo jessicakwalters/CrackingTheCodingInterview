d042a981a5ba77e39a529d01bbe011b764ee8d28
jessicakwalters <jessicakwalters@gmail.com>
2020-01-06 12:57:49 -0800

    validate BST

--- Binary_Trees/validate-bst.js
+++ Binary_Trees/validate-bst.js
@@ -4,12 +4,22 @@ const BinaryTree = require('./Binary-Tree');
 const BTNode = require('./Node');
 
 function isBST(node){
-  
+ if(!node.left && !node.right){
+   return true;
+ }
+ else if (node.left && node.left.value < node.value){
+   return isBST(node.left);
+ } else if (node.right && node.right.value > node.value){
+   return isBST(node.right);
+ } else {
+   return false;
+ }
 }
 
 let tree = new BinaryTree();
 let ten = new BTNode(10);
 tree.root = ten;
+let fifteen = new BTNode(15);
 let twenty = new BTNode(20);
 let thirty = new BTNode(30);
 let forty = new BTNode(40);
@@ -17,13 +27,16 @@ let one = new BTNode(1);
 let two = new BTNode(2);
 let three = new BTNode(3);
 let four = new BTNode(4);
+let five = new BTNode(5);
+let six = new BTNode(6);
 
-ten.right = one;
-ten.left = twenty;
-one.right = two;
-// two.left = three;
-// three.right = four;
+ten.right = twenty;
+ten.left = four;
+four.right = five;
+four.left = three;
+// three.left = six;
 twenty.right = thirty;
-twenty.left = forty;
+twenty.left = fifteen;
+thirty.right = forty;
 
-console.log(isBalanced(tree.root));
+console.log(isBST(tree.root));
